2e12d9e18b12616dea4f1891938ce8a33ba32b1f
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-11-07 20:41:22 -0500

    js in html

--- restaurantexpress.js
+++ restaurantexpress.js
@@ -4,22 +4,38 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var reservations = [];
-var waitlist= [];
+var tables = [];
+var waitlist = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
-  });
-app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-  });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+app.get("/tables", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.get("/api/tables", function (req, res) {
+  return res.json(tables);
+});
+app.get("/api/waitlist", function (req, res) {
+  return res.json(waitlist);
+});
+
+app.post("/api/tables", function (req, res) {
+  var newReservation = req.body;
+  console.log(newReservation);
+
+  tables.push(newReservation);
+
+  res.json(newReservation);
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
